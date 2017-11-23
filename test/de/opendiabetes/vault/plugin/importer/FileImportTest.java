4ab24439107d101a828a5651cff0b4922253a3ec
Lucas Buschlinger <studium@lucas-buschlinger.de>
2017-11-24 00:36:32 +0100

    Put Junit and Hamcrest JARs into testlib folder for CI, made plugin path platform independent

--- test/de/opendiabetes/vault/plugin/importer/FileImportTest.java
+++ test/de/opendiabetes/vault/plugin/importer/FileImportTest.java
@@ -1,43 +1,36 @@
 package de.opendiabetes.vault.plugin.importer;
 
-import de.opendiabetes.vault.importer.Importer;
+
 import org.junit.Assert;
 import org.junit.Test;
 import org.pf4j.DefaultPluginManager;
 import org.pf4j.PluginManager;
-import org.pf4j.PluginWrapper;
-
-import java.io.File;
-import java.io.IOException;
-import java.net.URI;
-import java.nio.file.*;
-import java.util.Iterator;
-
+import java.nio.file.Paths;
 
 public class FileImportTest {
     @Test
-    public void pluginLoad(){
-        PluginManager manager = new DefaultPluginManager(Paths.get("/home/magnus/OpenDiabetes/ourPlugins"));
+    public void pluginLoad() {
+        PluginManager manager = new DefaultPluginManager(Paths.get("ourPlugins"));
         manager.loadPlugins();
-        Assert.assertEquals(1,manager.getPlugins().size());
+        Assert.assertEquals(1, manager.getPlugins().size());
     }
 
     @Test
-    public void pluginStart(){
-        PluginManager manager = new DefaultPluginManager(Paths.get("/home/magnus/OpenDiabetes/ourPlugins"));
+    public void pluginStart() {
+        PluginManager manager = new DefaultPluginManager(Paths.get("ourPlugins"));
         manager.loadPlugins();
         manager.startPlugins();
-        Assert.assertEquals(1,manager.getStartedPlugins().size());
+        Assert.assertEquals(1, manager.getStartedPlugins().size());
     }
 
-   @Test
-    public void callPlugin(){
-       PluginManager manager =new DefaultPluginManager(Paths.get("/home/magnus/OpenDiabetes/ourPlugins"));
-       manager.loadPlugins();
-       manager.startPlugins();
-       System.out.println(manager.getExtensionClassNames("FileImporter"));
-       System.out.println(manager.getExtensions(de.opendiabetes.vault.plugin.importer.FileImporter.class));
+    @Test
+    public void callPlugin() {
+        PluginManager manager = new DefaultPluginManager(Paths.get("ourPlugins"));
+        manager.loadPlugins();
+        manager.startPlugins();
+        System.out.println(manager.getExtensionClassNames("FileImporter"));
+        System.out.println(manager.getExtensions(de.opendiabetes.vault.plugin.importer.FileImporter.class));
 
-   }
+    }
 
 }
