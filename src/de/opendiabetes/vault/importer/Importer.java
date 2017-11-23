b1385fac7780542332b5f84f873922242295c12b
Unknown <magnusgaertner@gmail.com>
2017-11-23 20:15:58 +0100

    created first plugin tests,  added container class from old project

--- src/de/opendiabetes/vault/importer/Importer.java
+++ src/de/opendiabetes/vault/importer/Importer.java
@@ -1,4 +1,23 @@
 package de.opendiabetes.vault.importer;
 
-public interface Importer {
+import de.opendiabetes.vault.container.RawEntry;
+import de.opendiabetes.vault.container.VaultEntry;
+import org.pf4j.ExtensionPoint;
+
+import java.util.List;
+
+/**
+ *
+ */
+
+public interface Importer extends ExtensionPoint{
+
+    public abstract boolean importData(String path);
+
+    public List<VaultEntry> getImportedData();
+
+    public List<RawEntry> getImportedRawData();
+
+    //public Logger getLogger();
+
 }
