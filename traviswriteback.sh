df61f99b0eee8e21add1c2c34dfc1695807f5b3c
Lucas Buschlinger <studium@lucas-buschlinger.de>
2017-11-28 17:23:47 +0100

    Trying writeback for reports

--- traviswriteback.sh
+++ traviswriteback.sh
@@ -12,10 +12,11 @@ commit_website_files() {
 }
 
 upload_files() {
-  git remote add BP https://${PUSH_BACK}@github.com:lucasbuschlinger/BachelorPraktikum.git > /dev/null 2>&1
+  git remote add BP ${PUSH_BACK}@github.com:lucasbuschlinger/BachelorPraktikum.git > /dev/null 2>&1
   git push --quiet --set-upstream BP feature/tool
 }
 
+echo ${echoss}
 setup_git
 commit_website_files
 upload_files
