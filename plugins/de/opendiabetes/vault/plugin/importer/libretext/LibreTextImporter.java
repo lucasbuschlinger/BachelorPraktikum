a17147a23e5613b237fe12ccecac409879700b8d
Lucas Buschlinger <studium@lucas-buschlinger.de>
2017-12-01 20:21:47 +0100

    First work on LibreText, Code missing.

---
+++ plugins/de/opendiabetes/vault/plugin/importer/libretext/LibreTextImporter.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2017 OpenDiabetes
+ * <p>
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * <p>
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.opendiabetes.vault.plugin.importer.libretext;
+
+import org.pf4j.Extension;
+import org.pf4j.Plugin;
+import org.pf4j.PluginWrapper;
+
+/**
+ * Wrapper class for the LibreTextImporter plugin.
+ */
+public class LibreTextImporter extends Plugin {
+
+    /**
+     * Constructor for the PluginManager.
+     * @param wrapper The PluginWrapper.
+     */
+    public LibreTextImporter(PluginWrapper wrapper) {
+        super(wrapper);
+    }
+
+    /**
+     * Actual implementation of the LibreText importer plugin.
+     */
+    @Extension
+    public static class LibreTextImporterImplementation {
+        
+    }
+}
---
+++ plugins/de/opendiabetes/vault/plugin/importer/libretext/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * This package contains the LibreText importer plugin.
+ */
+package de.opendiabetes.vault.plugin.importer.libretext;
