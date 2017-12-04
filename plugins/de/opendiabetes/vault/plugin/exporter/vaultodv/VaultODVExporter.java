5fcc57ab10011b6b1605219d157067634a9d8864
Tina Lu <olofkaspersen@gmail.com>
2017-12-04 13:29:42 +0100

    us8 implemented, needs a code review

---
+++ plugins/de/opendiabetes/vault/plugin/exporter/vaultodv/VaultODVExporter.java
@@ -0,0 +1,11 @@
+package de.opendiabetes.vault.plugin.exporter.vaultodv;
+
+/**
+ * not implemented yet
+ * only a dummy class for the importer
+ */
+public class VaultODVExporter {
+
+    public static final String DATA_ZIP_ENTRY = "data.csv";
+    public static final String SIGNATURE_ZIP_ENTRY = "sig.txt";
+}
