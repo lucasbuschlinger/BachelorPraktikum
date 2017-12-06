44f107390e90ea60338b88130522d4a06bc6ef52
Magnus Gärtner <magnusgaertner@gmail.com>
2017-12-06 08:50:04 +0100

    asserted loading only one plugin with helper TestImporterUtil.getImporter

--- test/de/opendiabetes/tests/plugin/importer/TestImporterUtil.java
+++ test/de/opendiabetes/tests/plugin/importer/TestImporterUtil.java
@@ -1,6 +1,7 @@
 package de.opendiabetes.tests.plugin.importer;
 
 import de.opendiabetes.vault.plugin.importer.Importer;
+import org.junit.Assert;
 import org.junit.Ignore;
 import org.pf4j.DefaultPluginManager;
 import org.pf4j.PluginManager;
@@ -13,6 +14,9 @@ public class TestImporterUtil {
         PluginManager manager = new DefaultPluginManager(Paths.get("export"));
         manager.loadPlugins();
         manager.startPlugin(importer);
+        Assert.assertEquals(1, manager.getStartedPlugins().size());
+        Assert.assertEquals(1, manager.getExtensions(Importer.class).size());
         return manager.getExtensions(Importer.class).get(0);
+
     }
 }
