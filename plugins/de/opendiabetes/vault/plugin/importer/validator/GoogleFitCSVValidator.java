c503983356dfcaab4c425fe8c2669963ff773995
Lucas Buschlinger <studium@lucas-buschlinger.de>
2018-01-11 17:42:41 +0100

    Principal implementation of english support: GoogleFit

--- plugins/de/opendiabetes/vault/plugin/importer/validator/GoogleFitCSVValidator.java
+++ plugins/de/opendiabetes/vault/plugin/importer/validator/GoogleFitCSVValidator.java
@@ -23,6 +23,7 @@ import java.io.File;
 import java.io.IOException;
 import java.text.ParseException;
 import java.util.Date;
+import java.util.logging.Logger;
 
 /**
  * Validator for Google Fit CSV data.
@@ -74,15 +75,44 @@ public class GoogleFitCSVValidator extends CSVValidator {
     };
 
     /**
+     * Field of the English Google Fit CSV header containing the start time of an activity.
+     */
+    private static final String HEADER_START_TIME_EN = "Start time";
+    /**
+     * Field of the English Google Fit CSV header containing the end time of an activity.
+     */
+    private static final String HEADER_END_TIME_EN = "End time";
+    /**
+     * Field of the English Google Fit CSV header containing biking duration.
+     */
+    private static final String HEADER_BIKE_VALUE_EN = "Cycling duration (ms)";
+    /**
+     * Field of the English Google Fit CSV header containing walking duration.
+     */
+    private static final String HEADER_WALK_VALUE_EN = "Walking duration (ms)";
+    /**
+     * Field of the English Google Fit CSV header containing running duration.
+     */
+    private static final String HEADER_RUN_VALUE_EN = "Running duration (ms)";
+    /**
+     * Field of the English Google Fit CSV header containing maximum velocity.
+     */
+    private static final String HEADER_MAX_SPEED_VALUE_EN = "Max speed (m/s)";
+    /**
+     * Time format used Google Fit CSV data.
+     */
+    private static final String TIME_FORMAT_EN = "yyyy-MM-dd HHmmss.SSSZ";
+
+    /**
      * The composed English header used in Google fit CSV data.
      */
     // TODO add english header
     public static final String[] HEADER_EN = {
-            HEADER_START_TIME_DE, HEADER_END_TIME_DE,
-            HEADER_BIKE_VALUE_DE,
-            HEADER_WALK_VALUE_DE,
-            HEADER_RUN_VALUE_DE,
-            HEADER_MAX_SPEED_VALUE_DE
+            HEADER_START_TIME_EN, HEADER_END_TIME_EN,
+            HEADER_BIKE_VALUE_EN,
+            HEADER_WALK_VALUE_EN,
+            HEADER_RUN_VALUE_EN,
+            HEADER_MAX_SPEED_VALUE_EN
     };
 
     /**
@@ -101,7 +131,20 @@ public class GoogleFitCSVValidator extends CSVValidator {
      */
     // TODO add language selection
     public long getBikeValue(final CsvReader creader) throws IOException {
-        String tmpValue = creader.get(HEADER_BIKE_VALUE_DE);
+        Language language = getLanguageSelection();
+        String tmpValue;
+        switch (language) {
+            case DE:
+                tmpValue = creader.get(HEADER_BIKE_VALUE_DE);
+                break;
+            case EN:
+                tmpValue = creader.get(HEADER_BIKE_VALUE_EN);
+                break;
+            default:
+                Logger.getLogger(this.getClass().getName()).severe("ASSERTION ERROR!");
+                throw new AssertionError();
+        }
+
         if (tmpValue != null && !tmpValue.isEmpty()) {
             return Long.parseLong(tmpValue);
         } else {
@@ -117,7 +160,19 @@ public class GoogleFitCSVValidator extends CSVValidator {
      * @throws IOException Thrown when reading the data goes wrong.
      */
     public long getWalkValue(final CsvReader creader) throws IOException {
-        String tmpValue = creader.get(HEADER_WALK_VALUE_DE);
+        Language language = getLanguageSelection();
+        String tmpValue;
+        switch (language) {
+            case DE:
+                tmpValue = creader.get(HEADER_WALK_VALUE_DE);
+                break;
+            case EN:
+                tmpValue = creader.get(HEADER_WALK_VALUE_EN);
+                break;
+            default:
+                Logger.getLogger(this.getClass().getName()).severe("ASSERTION ERROR!");
+                throw new AssertionError();
+        }
         if (tmpValue != null && !tmpValue.isEmpty()) {
             return Long.parseLong(tmpValue);
         } else {
@@ -133,7 +188,19 @@ public class GoogleFitCSVValidator extends CSVValidator {
      * @throws IOException Thrown when reading the data goes wrong.
      */
     public long getRunValue(final CsvReader creader) throws IOException {
-        String tmpValue = creader.get(HEADER_RUN_VALUE_DE);
+        Language language = getLanguageSelection();
+        String tmpValue;
+        switch (language) {
+            case DE:
+                tmpValue = creader.get(HEADER_RUN_VALUE_DE);
+                break;
+            case EN:
+                tmpValue = creader.get(HEADER_RUN_VALUE_EN);
+                break;
+            default:
+                Logger.getLogger(this.getClass().getName()).severe("ASSERTION ERROR!");
+                throw new AssertionError();
+        }
         if (tmpValue != null && !tmpValue.isEmpty()) {
             return Long.parseLong(tmpValue);
         } else {
@@ -149,7 +216,19 @@ public class GoogleFitCSVValidator extends CSVValidator {
      * @throws IOException Thrown when reading the data goes wrong.
      */
     public double getMaxSpeedValue(final CsvReader creader) throws IOException {
-        String tmpValue = creader.get(HEADER_MAX_SPEED_VALUE_DE);
+        Language language = getLanguageSelection();
+        String tmpValue;
+        switch (language) {
+            case DE:
+                tmpValue = creader.get(HEADER_MAX_SPEED_VALUE_DE);
+                break;
+            case EN:
+                tmpValue = creader.get(HEADER_MAX_SPEED_VALUE_EN);
+                break;
+            default:
+                Logger.getLogger(this.getClass().getName()).severe("ASSERTION ERROR!");
+                throw new AssertionError();
+        }
         if (tmpValue != null && !tmpValue.isEmpty()) {
             return Double.parseDouble(tmpValue);
         } else {
@@ -167,7 +246,16 @@ public class GoogleFitCSVValidator extends CSVValidator {
      * @throws ParseException Thrown when a {@link TimestampUtils#createCleanTimestamp(String, String)} can not be created.
      */
     public long getStartTime(final CsvReader creader, final String fileName) throws IOException, ParseException {
-        return timestampHack(creader.get(HEADER_START_TIME_DE), fileName);
+        Language language = getLanguageSelection();
+        switch (language) {
+            case DE:
+                return timestampHack(creader.get(HEADER_START_TIME_DE), fileName, language);
+            case EN:
+                return timestampHack(creader.get(HEADER_START_TIME_EN), fileName, language);
+            default:
+                Logger.getLogger(this.getClass().getName()).severe("ASSERTION ERROR!");
+                throw new AssertionError();
+        }
     }
 
     /**
@@ -180,7 +268,16 @@ public class GoogleFitCSVValidator extends CSVValidator {
      * @throws ParseException Thrown when a {@link TimestampUtils#createCleanTimestamp(String, String)} can not be created.
      */
     public long getEndTime(final CsvReader creader, final String fileName) throws IOException, ParseException {
-        return timestampHack(creader.get(HEADER_END_TIME_DE), fileName);
+        Language language = getLanguageSelection();
+        switch (language) {
+            case DE:
+                return timestampHack(creader.get(HEADER_END_TIME_DE), fileName, language);
+            case EN:
+                return timestampHack(creader.get(HEADER_END_TIME_EN), fileName, language);
+            default:
+                Logger.getLogger(this.getClass().getName()).severe("ASSERTION ERROR!");
+                throw new AssertionError();
+        }
     }
 
     /**
@@ -188,18 +285,33 @@ public class GoogleFitCSVValidator extends CSVValidator {
      *
      * @param dbString The field to get the timestamp from.
      * @param fileName The name of the file to extract the time from.
+     * @param language The language used in the file.
      * @return The timestamp.
      * @throws ParseException Thrown when a {@link TimestampUtils#createCleanTimestamp(String, String)} can not be created.
      */
-    private long timestampHack(final String dbString, final String fileName) throws ParseException {
+    private long timestampHack(final String dbString, final String fileName, final Language language)
+            throws ParseException {
         if (dbString == null || dbString.isEmpty()) {
             return 0;
         }
         String tempFileName = new File(fileName).getName();
         String dateString = tempFileName.split("\\.")[0]; //dirty hack --> change to json would solve this problem
-        Date timestamp = TimestampUtils.createCleanTimestamp(
-                dateString + " " + dbString.replaceAll(":", ""),
-                TIME_FORMAT_DE);
+        Date timestamp;
+        switch (language) {
+            case DE:
+                timestamp = TimestampUtils.createCleanTimestamp(
+                        dateString + " " + dbString.replaceAll(":", ""),
+                        TIME_FORMAT_DE);
+                break;
+            case EN:
+                timestamp = TimestampUtils.createCleanTimestamp(
+                        dateString + " " + dbString.replaceAll(":", ""),
+                        TIME_FORMAT_EN);
+                break;
+            default:
+                Logger.getLogger(this.getClass().getName()).severe("ASSERTION ERROR!");
+                throw new AssertionError();
+        }
         return timestamp.getTime();
     }
 }
