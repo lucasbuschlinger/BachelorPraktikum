8409b1998a0be0ba980d90be0e53c03d57dfacae
Magnus Gärtner <magnusgaertner@gmail.com>
2018-02-10 16:52:20 +0100

    first local test on import and interpret to db. seems to work :)

--- test/de/opendiabetes/tests/plugin/interpreter/NonInterpreterTest.java
+++ test/de/opendiabetes/tests/plugin/interpreter/NonInterpreterTest.java
@@ -1,16 +1,23 @@
 package de.opendiabetes.tests.plugin.interpreter;
 
 import de.opendiabetes.tests.plugin.util.TestUtil;
+import de.opendiabetes.vault.data.VaultDao;
 import de.opendiabetes.vault.plugin.common.OpenDiabetesPlugin;
 import de.opendiabetes.vault.plugin.importer.Importer;
 import de.opendiabetes.vault.plugin.interpreter.Interpreter;
-import org.junit.Assert;
+import de.opendiabetes.vault.plugin.management.ImportAndInterpretDataflowWrapper;
 import org.junit.Test;
 import org.pf4j.DefaultPluginManager;
 import org.pf4j.PluginManager;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.nio.file.Paths;
+import java.sql.SQLException;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Properties;
 
 public class NonInterpreterTest {
 
@@ -23,7 +30,7 @@ public class NonInterpreterTest {
         PluginManager manager = new DefaultPluginManager(Paths.get("export"));
         manager.loadPlugins();
         manager.startPlugins();
-        Interpreter interpreter = manager.getExtensions(Interpreter.class, "PumpInterpreter").get(0);
+        Interpreter interpreter = manager.getExtensions(Interpreter.class, "NonInterpreter").get(0);
 
     }
 
@@ -34,5 +41,45 @@ public class NonInterpreterTest {
     public void interpretTest() {
         Interpreter interpreter = TestUtil.getInterpreter("NonInterpreter");
         interpreter.interpret(null);
+    }
+
+    @Test
+    public void importAndInterpretTest() throws IOException, SQLException {
+        VaultDao.initializeDb();
+        VaultDao db = VaultDao.getInstance();
+
+        ImportAndInterpretDataflowWrapper manager = new ImportAndInterpretDataflowWrapper();
+        Importer medImporter = TestUtil.getImporter("MedtronicImporter");
+        medImporter.setImportFilePath("/home/magnus/testdata/CareLink-Export-1486459734778.csv");
+
+        OpenDiabetesPlugin.StatusListener listener = new OpenDiabetesPlugin.StatusListener() {
+            @Override
+            public void onStatusCallback(int progress, String status) {
+                System.out.println(progress+status);
+            }
+        };
+
+        medImporter.registerStatusCallback(listener);
+
+        Interpreter dateInterpreter = TestUtil.getInterpreter("DateInterpreter");
+        dateInterpreter.registerStatusCallback(listener);
+
+        Properties dateInterpreterProperties = new Properties();
+        dateInterpreterProperties.load(new FileInputStream("/home/magnus/OpenDiabetes/properties/dateInterpreter.properties"));
+        if (!dateInterpreter.loadConfiguration(dateInterpreterProperties)) return;
+        dateInterpreter.init(db);
+
+        System.out.println("date interpreter load configuration successful");
+
+        Interpreter pumpInterpreter = TestUtil.getInterpreter("PumpInterpreter");
+        Properties pumpInterpreterProperties = new Properties();
+
+
+        pumpInterpreterProperties.load(new FileInputStream("/home/magnus/OpenDiabetes/properties/pumpInterpreter.properties"));
+        if (!pumpInterpreter.loadConfiguration(pumpInterpreterProperties)) return;
+        pumpInterpreter.init(db);
+        System.out.println("pump interpreter load configuration successful");
+        Interpreter[] interpreters = new Interpreter[]{dateInterpreter, pumpInterpreter};
+        manager.importAndInterpretWithoutDb(medImporter, Arrays.asList(interpreters)).forEach(x->System.out.println(x));
     }
 }
