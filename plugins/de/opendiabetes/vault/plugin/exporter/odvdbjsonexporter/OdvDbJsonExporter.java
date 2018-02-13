f54bd2414da7e2b91a8ae6524927e937b961eed9
Lucas Buschlinger <studium@lucas-buschlinger.de>
2018-02-13 17:54:51 +0100

    Revert "Small stash"
    This reverts commit 0114ac8ec6d27a3d390d3232f822f3be6a06a23c.

--- plugins/de/opendiabetes/vault/plugin/exporter/odvdbjsonexporter/OdvDbJsonExporter.java
+++ plugins/de/opendiabetes/vault/plugin/exporter/odvdbjsonexporter/OdvDbJsonExporter.java
@@ -70,14 +70,20 @@ public class OdvDbJsonExporter extends Plugin {
         }
 
         /**
-         * Unused, thus unimplemented.
+         * Setter for an object used by the specific exporters.
+         * The implementation of this should only call a private setter method like
+         * {@link de.opendiabetes.vault.plugin.exporter.VaultExporter#setDatabase(VaultDao)}.
+         * in the exporters and pass the object to those.
          *
-         * @param entries Nothing here.
-         * @throws IllegalArgumentException No thrown as this will not change the state of the exporter.
+         * @param object The object to be set.
+         * @throws IllegalArgumentException Thrown if a wrong object gets passed (for example if a {@link VaultDao} object gets passed
+         *                                  to a {@link de.opendiabetes.vault.plugin.exporter.slicelayoutcsv.SliceLayoutCSVExporter} which
+         *                                  only takes a list of {@link de.opendiabetes.vault.container.SliceEntry}.
          */
         @Override
-        public void setEntries(final List<?> entries) throws IllegalArgumentException {
-            LOG.log(Level.WARNING, "Tried to set entries but this it not possible with this exporter");
+        public void setAdditional(final Object object) throws IllegalArgumentException {
+            LOG.log(Level.WARNING, "Unpossible to set additional for OdvDbJsonExporter because "
+                    + "the exporter does not have any additional objects to overwrite.");
         }
 
         /**
